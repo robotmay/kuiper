f062be46d3b7cce568a7558b2b871b51ecded6f3
Robert May <robotmay@gmail.com>
2012-12-12 08:54:28 +0000

    Setting queue priority

--- app/workers/tracker_worker.rb
+++ app/workers/tracker_worker.rb
@@ -1,12 +1,12 @@
 class TrackerWorker
   include Sidekiq::Worker
+  sidekiq_options queue: :incoming
 
   def perform(client_data, server_data)
     client_data = JSON.parse(client_data, symbolize_names: true)
     server_data = JSON.parse(server_data, symbolize_names: true)
     
     payload = client_data.merge(server_data)
-    puts "API Key: #{payload.inspect}"
     Visit.create_from_client_data(payload)
   end
 end
